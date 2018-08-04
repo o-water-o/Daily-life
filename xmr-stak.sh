43524d8c3ebcfaa3a9773847fd91658196385766
o-water-o <457331510@qq.com>
2018-08-04 17:45:37 +0800

    Create xmr-stak.sh

---
+++ xmr-stak.sh
@@ -0,0 +1,7 @@
+sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+git clone https://github.com/fireice-uk/xmr-stak.git
+mkdir xmr-stak/build
+cd xmr-stak/build
+cmake .. -DCUDA_ENABLE=OFF \
+         -DOpenCL_ENABLE=OFF 
+make install
