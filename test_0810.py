37b6b4804ea5006272cd96e52ec194f40dbf2108
o-water-o <457331510@qq.com>
2018-08-11 17:12:35 +0800

    Update test_0810.py

--- test_0810.py
+++ test_0810.py
@@ -1,50 +1,66 @@
-import re,requests,json,time,os
+# coding = utf-8
+import requests,time,re,os,time
+import pdb
 from lxml import etree
 
-'''
-t = "20180809/腾讯课堂大师班 | 尝尝学习的甜头.jpg"
-t0 = re.sub(r'[/|]+','_',t)
-print(t0)
-'''
-'''
-url = 'http://github.com'
-r = requests.get(url)
-print(r.status_code)
-data = json.dumps(dict(r.headers),indent=4) ##indent=4,separators=(',', ': '),ensure_ascii=False
-print(data)
-print(r.cookies)
-print('##########################################################################')
-for cookie in r.cookies.keys():
-	print(cookie+':'+r.cookies.get(cookie))
-	print('----------------------------------------------------------------------')
-'''
-##url_login = 'https://ke.qq.com/'
+count = 1
+def download(i):
+	global count
+	headers = {'content-type': 'application/json',
+	           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+	url = 'https://ke.qq.com/course/list/%E5%8F%A3%E8%85%94%E5%8C%BB%E5%AD%A6'
+	url1 = 'https://ke.qq.com/course/list?mt=1004&st=2039&task_filter=0000000&&page={}'.format(i)
+	req = requests.get(url1)
+	selector = etree.HTML(req.content)
 
-def write_file(text,name):
-	dir = time.strftime("%Y%m%d")
-	if not os.path.exists((dir)):
-		os.makedirs((dir))	
-	with open(r'%s/%s.txt' % (dir,  re.sub(r'[/|]+','',name)),'a',encoding='utf-8') as f:
-		f.write(text+'\n')
+	xp0 = selector.xpath('//a[@cors-name="course"]/img/@title')
+	xp1 = selector.xpath('//a[@cors-name="course"]/img/@src')
+	xp3 = selector.xpath('//a[@cors-name="course"]')
 
-
-def sub_page(url):
-	s = requests.session()
-	url = "https://ke.qq.com/course/69943"
-	r = s.get(url) 
-	# print(str(r.status_code)+'\n'+json.dumps(dict(r.request.headers),indent=4))
-
-	selector = etree.HTML(r.content)
-	xp0 = selector.xpath("//div[@class='intro-course']/div/text()")
-	for i in xp0:
-		name = selector.xpath("//span[@class='title-main']/text()")
+	'''with open('text1.txt','w+') as f:
+		for i in xp0:
+			f.write(i+'\n')
+	'''
+	print(len(xp1))
+	for j in range(1,len(xp1)):
 		
-		#print(re.sub(r'\n','',i))
-		write_file(re.sub(r'\n','',i),name[0])
-	xp1 = selector.xpath("//div[@class='task-part-list']//h3[@class='part-tt']")
-def index_page():
-	s = requests.session()
+		u = xp1[j]
+		u = re.sub('//','http://',u)
+		name = xp0[j]
+		print(name+'\t'+u)
+		print(name+'\t'+re.sub(r'[0-9]{3}', '510', u))
+		# 
+		
+		
+		
+		#if(requests.get(re.sub(r'[0-9]{3}', '510', u),headers=headers).content):
+		img = requests.get(re.sub(r'[0-9]{3}', '510', u),headers=headers).content
+		print(len(str(img)+'\n'+'success')	)
+			
 
+		# pdb.set_trace()		
+		
+		dir = time.strftime("%Y%m%d")
+		if not os.path.exists((dir)):
+			os.makedirs((dir))
+		try:
+			with open(r'%s/%s.jpg' % (dir,  re.sub(r'[/|\v，]+','_',name)), 'wb') as f:
+			#with open(str(j)+'.png','wb+') as f:
+				f.write(img)	
+				# pdb.set_trace()
 
+			
+			print("第 %d 个下载成功  "%count)
+			count +=1
+				#print(i)
+		except:
+			print('下载失败')
+		
+		
+		
+	print("第 %s 页  "%i)
+	time.sleep(1)
+	##http://10.url.cn/qqcourse_logo_ng/ajNVdqHZLLBsiadlyUgak8bicMdqiaCmqdX7tPAuBwX0a3ceT7hNhibV8Kr5ukCeDU8edZkMFfFq76I/510
 if __name__ == '__main__':
-	sub_page()
+	for i in range(1,20):
+		download(i)
